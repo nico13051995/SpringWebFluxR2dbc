3f67a66d050004aed35dab88d946395da45c91c7
Roman zinchuk <zigzag2341@gmail.com>
2019-02-01 15:49:31 +0200

    //

--- src/main/resources/db/migration/V1__Initial_version.sql
+++ src/main/resources/db/migration/V1__Initial_version.sql
@@ -1,13 +1,17 @@
--- Table: public.tbl_user
+-- Table: public."user"
 
--- DROP TABLE public.tbl_user;
+-- DROP TABLE public."user";
 
-CREATE TABLE public.tbl_user
+CREATE TABLE public."user"
 (
-     id SERIAL PRIMARY KEY,
-    name text
+    id bigint NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+    name character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT user_pkey PRIMARY KEY (id)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
+
+ALTER TABLE public."user"
+    OWNER to postgres;
